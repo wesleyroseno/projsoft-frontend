53fa40e8a1cced031d3d519555d9a67ca4e576b5
Wesley Roseno <wesley.saraiva@ccc.ufcg.edu.br>
2019-07-06 13:06:07 -0300

    Likes em disciplinas implementado

--- javascript/index.js
+++ javascript/index.js
@@ -5,6 +5,8 @@ var loginModalBtn = document.getElementById("loginModalButton");
 var loginModalClose = document.getElementById("loginModalClose");
 var logged = document.getElementById("logged");
 var logoutButton = document.getElementById("logoutButton");
+var containerCourseModal = document.getElementById("containerCourseModal");
+
 
 loginModalBtn.onclick = function () {
     loginModal.style.display = "block";
@@ -24,6 +26,13 @@ createUserModalBtn.onclick = function () {
 
 createUserModalClose.onclick = function () {
     createUserModal.style.display = "none";
+}
+
+var courseModal = document.getElementById("courseModal");
+var courseModalClose = document.getElementById("courseModalClose");
+
+courseModalClose.onclick = function () {
+    courseModal.style.display = "none";
 }
 
 var aboutButton = document.getElementById("aboutButton");
@@ -60,9 +69,13 @@ window.onclick = function (event) {
     if (event.target == createUserModal) {
         createUserModal.style.display = "none";
     }
+    if (event.target == courseModal) {
+        courseModal.style.display = "none";
+    }
 }
 
 var coursesLayout = document.getElementById("coursesLayout")
+
 
 async function fetchCourses(query) {
     if (query) {
@@ -79,8 +92,14 @@ function loadCourses(courses) {
     coursesLayout.innerHTML = '';
     courses.forEach(course => {
         coursesLayout.innerHTML +=
-            `<div class="course-card"><p>${course.id} - ${course.name}</p></div>`;
-    })
+            `<div class="course-card" id="course-card-${course.id}"><p>${course.id} - ${course.name}</p></div>`;
+    });
+    var courseCards = document.getElementsByClassName("course-card");
+
+    for (let index = 0; index < courseCards.length; index++) {
+        var element = courseCards[index];
+        element.addEventListener('click', openCourseModal, false);
+    }
 }
 
 //setup before functions
@@ -118,8 +137,9 @@ logoutButton.onclick = function () {
 
     logoutButton.style.display = "none";
     logged.style.display = "none";
-    logged.innerHTML = ``; 
+    logged.innerHTML = ``;
     localStorage.removeItem('token');
+    localStorage.removeItem('userEmail');
 }
 
 async function sendLogin(user) {
@@ -127,9 +147,9 @@ async function sendLogin(user) {
         let response = await fetch(baseurl + "/v1/auth/login", {
             method: 'POST',
             body: JSON.stringify(user),
-            headers:{
-                'Access-Control-Allow-Origin':'*',
-                'Content-Type':'application/json;charset=utf-8'
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json;charset=utf-8'
             }
         });
 
@@ -137,6 +157,7 @@ async function sendLogin(user) {
 
         if (response.status == 200) {
             localStorage.setItem('token', json.token);
+            localStorage.setItem('userEmail', user.email);
 
             loginModal.style.display = "none";
             document.getElementById("loginEmail").value = "";
@@ -151,6 +172,7 @@ async function sendLogin(user) {
         }
         else {
             localStorage.removeItem('token');
+            localStorage.removeItem('userEmail');
             alert(json.message);
         }
     }
@@ -175,9 +197,9 @@ async function createUser(user) {
         let response = await fetch(baseurl + "/v1/users/", {
             method: 'POST',
             body: JSON.stringify(user),
-            headers:{
-                'Access-Control-Allow-Origin':'*',
-                'Content-Type':'application/json;charset=utf-8'
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json;charset=utf-8'
             }
         });
 
@@ -202,3 +224,93 @@ async function createUser(user) {
 }
 
 document.getElementById("createUserButton").addEventListener("click", sendCreateUserRequest, false);
+
+async function openCourseModal() {
+    var courseId = this.id.split("-")[2];
+
+    try {
+        var response2 = await fetch(baseurl + "/v1/courses/private/" + courseId, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem("token")
+            }
+        });
+
+        var json2 = await response2.json();
+
+        if (response2.status == 200) {
+            courseModal.style.display = "block";
+            createCourseModal(json2);
+        }
+        else {
+            alert(json2.message);
+        }
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+
+function userLikes(courseProfile) {
+    for (let i = 0; i < courseProfile.likes.length; i++) {
+        const element = courseProfile.likes[i];
+        if ( element.email === localStorage.getItem("userEmail")) {
+            return true;
+        }
+    }
+}
+
+function mountProfile(courseProfile) {
+    var likesCount = courseProfile.likes.length;
+    var likeMsg = "Dar like";
+    if (userLikes(courseProfile)) {
+        likeMsg = "Retirar like";
+    }
+
+    var likesStr = "";
+    var separator = "";
+    for (let i = 0; i < courseProfile.likes.length; i++) {
+        const element = courseProfile.likes[i];
+        likesStr += separator + element.firstName + " " + element.lastName + " (" + element.email + ")";
+        separator = ", ";
+    }
+    likesStr += ".";
+    containerCourseModal.innerHTML = `<h1>${courseProfile.id} - ${courseProfile.name}</h1>
+    <hr>
+    <p>${likesCount} likes</p>
+    <div id="likes-container">${likesStr}</div>
+    <button id="likeButton">${likeMsg}</button>
+    <hr>
+    <label for="comments"><b>Comentários</b></label>
+    <input type="text" placeholder="Comente algo sobre a disciplina" name="comments" id="comments">`;
+}
+
+function createCourseModal(courseProfile) {
+    mountProfile(courseProfile);
+
+    var likeButton = document.getElementById("likeButton");
+    likeButton.addEventListener('click', _ => sendLike(courseProfile), false);
+}
+
+async function sendLike(courseProfile) {
+    try {
+        var response2 = await fetch(baseurl + "/v1/courses/private/" + courseProfile.id + "/like", {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem("token")
+            }
+        });
+
+        var json2 = await response2.json();
+
+        if (response2.status == 200) {
+            createCourseModal(json2);
+        }
+        else {
+            alert(json2.message);
+        }
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
