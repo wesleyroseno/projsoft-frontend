1c24e743d2142f8c8c5459f089a4c4282efeb7a8
Wener Castro <wenerwagner@gmail.com>
2019-07-06 07:09:28 -0300

    layout

--- javascript/view/index.js
+++ javascript/view/index.js
@@ -1,25 +1,58 @@
- // Get the modal
- var modal = document.getElementById("myModal");
+var loginModal = document.getElementById("loginModal");
+var loginModalBtn = document.getElementById("loginModalButton");
+var loginModalClose = document.getElementById("loginModalClose");
 
- // Get the button that opens the modal
- var btn = document.getElementById("myBtn");
+loginModalBtn.onclick = function () {
+    loginModal.style.display = "block";
+}
 
- // Get the <span> element that closes the modal
- var span = document.getElementsByClassName("close")[0];
+loginModalClose.onclick = function () {
+    loginModal.style.display = "none";
+}
 
- // When the user clicks on the button, open the modal
- btn.onclick = function () {
-     modal.style.display = "block";
- }
+var createUserModal = document.getElementById("createUserModal");
+var createUserModalBtn = document.getElementById("createUserModalButton");
+var createUserModalClose = document.getElementById("createUserModalClose");
 
- // When the user clicks on <span> (x), close the modal
- span.onclick = function () {
-     modal.style.display = "none";
- }
+createUserModalBtn.onclick = function () {
+    createUserModal.style.display = "block";
+}
 
- // When the user clicks anywhere outside of the modal, close it
- window.onclick = function (event) {
-     if (event.target == modal) {
-         modal.style.display = "none";
-     }
- } 
+createUserModalClose.onclick = function () {
+    createUserModal.style.display = "none";
+}
+
+var aboutButton = document.getElementById("aboutButton");
+var coursesButton = document.getElementById("coursesButton");
+var rankingButton = document.getElementById("rankingButton");
+
+var aboutPage = document.getElementById("about");
+var coursesPage = document.getElementById("courses");
+var rankingPage = document.getElementById("ranking");
+
+aboutButton.onclick = function () {
+    aboutPage.style.display = "block";
+    coursesPage.style.display = "none";
+    rankingPage.style.display = "none";
+}
+
+coursesButton.onclick = function () {
+    aboutPage.style.display = "none";
+    coursesPage.style.display = "block";
+    rankingPage.style.display = "none";
+}
+
+rankingButton.onclick = function () {
+    aboutPage.style.display = "none";
+    coursesPage.style.display = "none";
+    rankingPage.style.display = "block";
+}
+
+window.onclick = function (event) {
+    if (event.target == loginModal) {
+        loginModal.style.display = "none";
+    }
+    if (event.target == createUserModal) {
+        createUserModal.style.display = "none";
+    }
+} 
